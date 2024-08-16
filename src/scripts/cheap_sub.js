58032d2bea31548b0dd23dfcf91a913dc97e20ec
Tianze <92846194+tianze-hou@users.noreply.github.com>
2024-08-16 16:56:12 +0800

    Update cheap_sub.js

--- src/scripts/cheap_sub.js
+++ src/scripts/cheap_sub.js
@@ -4573,7 +4573,88 @@
     });
 
     option = {
-        // ... 你的ECharts配置选项 ...
+        title: {
+            top: 'top',
+            left: 'right',
+            textStyle: {
+                color: '#ffffff' // 设置全局默认文字颜色为白色
+            },
+        },
+        tooltip: {
+            formatter: function (graph) {
+                if (graph.dataType === 'node') {
+                    return graph.data.name;
+                } else if (graph.dataType === 'edge') {
+                    return graph.data.source + ' -> ' + graph.data.target;
+                }
+            }
+        },
+        legend: [
+            {
+                data: graph.categories.map(function (a) {
+                    return a.name;
+                }),
+                textStyle: {
+                    color: '#ffffff' // 设置全局默认文字颜色为白色
+                },
+            }
+
+        ],
+        animationDuration: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        series: [
+            {
+                type: 'graph',
+                layout: 'none',
+                legendHoverLink: false,
+                data: graph.nodes,
+                links: graph.links,
+                categories: graph.categories.map(function (category) {
+                    var colorMap = {
+                        "东北地区": "#5b8ff9",
+                        "华北地区": "#5ad8a6",
+                        "华中地区": "#f6bd16",
+                        "华东地区": "#e86452",
+                        "华南地区": "#6dc8ec",
+                        "西北地区": "#ff9845",
+                        "西南地区": "#1e9493"
+                    };
+                    return {
+                        name: category.name,
+                        itemStyle: {
+                            color: colorMap[category.name] || '#999999' // 默认颜色为黑色
+                        }
+                    };
+                }),
+                draggable: true,
+                label: {
+                    fontSize: '8',
+                    position: 'right',
+                    show: function () {
+                        // 根据节点的大小动态决定是否显示标签
+                        return this.symbolSize > 12;
+                    },
+                    formatter: '{b}' // 标签显示节点名称
+                },
+                edgeSymbol: ['circle', 'arrow'],
+                edgeSymbolSize: [4, 15],
+                lineStyle: {
+                    color: 'source',
+                    curveness: 0.35
+                },
+                emphasis: {
+                    focus: 'adjacency',
+                    label: {
+                        show: true, // 强调时显示标签
+                        position: 'right', // 标签位置
+                        formatter: '{b}' // 标签内容
+                    },
+                    lineStyle: {
+                        width: 20
+                    }
+                }
+            }
+        ]
     };
 
     myChart.hideLoading();
